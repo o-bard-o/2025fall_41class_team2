4c510418f93843ef73ff79e311e0904ac18cf2b0
o-bard-o <i4song.dev@gmail.com>
2025-11-25 19:02:40 +0900

    :wrench: server name configuration update

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-e6=d==o_qu-qon&7-@f&e6tg=9q7c%zq*q1#1qsajb&o2w=mn4
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    ".ap-northeast-2.compute.amazonaws.com", "localhost"
+    ".ap-northeast-2.compute.amazonaws.com", "localhost",
+    "api.sogong.me"
 ]
 
 
